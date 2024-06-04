a6ea590ef9bfc3e75a8386fe8f96da6912eb8a6e
gsjoy24 <gour.joy24@gmail.com>
2024-06-05 01:47:23 +0600

    added lost and found items on profile

--- src/app/my-profile/page.tsx
+++ src/app/my-profile/page.tsx
@@ -3,6 +3,7 @@ import PrivateRoute from '@/components/PrivateRoute/PrivateRoute';
 import LostItemCard from '@/components/Shared/LostItemCard/LostItemCard';
 import SectionTitle from '@/components/Shared/SectionTitle';
 import { useGetProfileQuery } from '@/redux/api/features/profileApi';
+import { TFoundItem } from '@/types/foundItem';
 import { TLostItem } from '@/types/lostItem';
 import DateToString from '@/utils/DateToString';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
@@ -10,8 +11,9 @@ import EmailIcon from '@mui/icons-material/Email';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import PersonIcon from '@mui/icons-material/Person';
 import UpdateIcon from '@mui/icons-material/Update';
-import { Box, Container, Divider, Grid, Stack, Typography } from '@mui/material';
+import { Box, Button, Container, Divider, Grid, Stack, Typography } from '@mui/material';
 import Image from 'next/image';
+import Link from 'next/link';
 import LoadingCompo from '../loading';
 
 const MyProfile = () => {
@@ -21,7 +23,7 @@ const MyProfile = () => {
 		return <LoadingCompo />;
 	}
 
-	const { user, bio, image, age, createdAt, lostItems, foundItems, claimedItems, counts } = data?.data;
+	const { user, bio, image, age, createdAt, lostItems, foundItems, counts } = data?.data;
 	const { name, email, phone, username } = user;
 	return (
 		<PrivateRoute>
@@ -45,145 +47,147 @@ const MyProfile = () => {
 							}
 						}}
 					>
-						<Image
-							src={image}
-							width={200}
-							height={200}
-							alt={user}
-							className='border-[6px] rounded-full border-[#1586FD] mb-2 mx-auto'
-						/>
+						<div className='static md:sticky top-[60px]'>
+							<Image
+								src={image}
+								width={200}
+								height={200}
+								alt={user}
+								className='border-[6px] rounded-full border-[#1586FD] mb-2 mx-auto'
+							/>
 
-						{/*name , username and bio in center  */}
+							{/*name , username and bio in center  */}
 
-						<Typography variant='h4' align='center'>
-							{name}
-						</Typography>
+							<Typography variant='h4' align='center'>
+								{name}
+							</Typography>
 
-						<Typography variant='body2' gutterBottom align='center' fontSize={14}>
-							<Box display='flex' alignItems='center' justifyContent='center'>
-								<AlternateEmailIcon sx={{ fontSize: '12px' }} />
-								{username}
+							<Typography variant='body2' gutterBottom align='center' fontSize={14}>
+								<Box display='flex' alignItems='center' justifyContent='center'>
+									<AlternateEmailIcon sx={{ fontSize: '12px' }} />
+									{username}
+								</Box>
+							</Typography>
+							<Typography variant='body1' gutterBottom align='center'>
+								{bio}
+							</Typography>
+
+							<Divider
+								sx={{
+									my: 2
+								}}
+							/>
+
+							<Box
+								display='flex'
+								alignItems='center'
+								justifyContent='space-between'
+								sx={{
+									my: 1
+								}}
+							>
+								<EmailIcon sx={{ mr: 1 }} />
+								<Typography>{email}</Typography>
 							</Box>
-						</Typography>
-						<Typography variant='body1' gutterBottom align='center'>
-							{bio}
-						</Typography>
 
-						<Divider
-							sx={{
-								my: 2
-							}}
-						/>
+							<Divider
+								sx={{
+									my: 2
+								}}
+							/>
 
-						<Box
-							display='flex'
-							alignItems='center'
-							justifyContent='space-between'
-							sx={{
-								my: 1
-							}}
-						>
-							<EmailIcon sx={{ mr: 1 }} />
-							<Typography>{email}</Typography>
-						</Box>
+							<Box
+								display='flex'
+								alignItems='center'
+								justifyContent='space-between'
+								sx={{
+									my: 1
+								}}
+							>
+								<LocalPhoneIcon sx={{ mr: 1 }} />
+								<Typography>{phone}</Typography>
+							</Box>
 
-						<Divider
-							sx={{
-								my: 2
-							}}
-						/>
+							<Divider
+								sx={{
+									my: 2
+								}}
+							/>
 
-						<Box
-							display='flex'
-							alignItems='center'
-							justifyContent='space-between'
-							sx={{
-								my: 1
-							}}
-						>
-							<LocalPhoneIcon sx={{ mr: 1 }} />
-							<Typography>{phone}</Typography>
-						</Box>
+							<Box
+								display='flex'
+								alignItems='center'
+								justifyContent='space-between'
+								sx={{
+									my: 1
+								}}
+							>
+								<PersonIcon sx={{ mr: 1 }} />
+								<Typography>{age} years</Typography>
+							</Box>
 
-						<Divider
-							sx={{
-								my: 2
-							}}
-						/>
+							<Divider
+								sx={{
+									my: 2
+								}}
+							/>
 
-						<Box
-							display='flex'
-							alignItems='center'
-							justifyContent='space-between'
-							sx={{
-								my: 1
-							}}
-						>
-							<PersonIcon sx={{ mr: 1 }} />
-							<Typography>{age} years</Typography>
-						</Box>
+							<Box
+								display='flex'
+								alignItems='center'
+								justifyContent='space-between'
+								sx={{
+									my: 1
+								}}
+							>
+								<UpdateIcon sx={{ mr: 1 }} />
+								<Typography>{DateToString(createdAt)}</Typography>
+							</Box>
 
-						<Divider
-							sx={{
-								my: 2
-							}}
-						/>
+							<Divider
+								sx={{
+									my: 2
+								}}
+							>
+								<span className='text-[10px]'>Counts</span>
+							</Divider>
 
-						<Box
-							display='flex'
-							alignItems='center'
-							justifyContent='space-between'
-							sx={{
-								my: 1
-							}}
-						>
-							<UpdateIcon sx={{ mr: 1 }} />
-							<Typography>{DateToString(createdAt)}</Typography>
-						</Box>
+							<Box
+								display='flex'
+								alignItems='center'
+								justifyContent='space-between'
+								sx={{
+									my: 1
+								}}
+							>
+								<Typography>Lost Items</Typography>
+								<Typography>{counts?.lostItems}</Typography>
+							</Box>
 
-						<Divider
-							sx={{
-								my: 2
-							}}
-						>
-							<span className='text-[10px]'>Counts</span>
-						</Divider>
+							<Box
+								display='flex'
+								alignItems='center'
+								justifyContent='space-between'
+								sx={{
+									my: 1
+								}}
+							>
+								<Typography>Found Items</Typography>
+								<Typography>{counts?.foundItems}</Typography>
+							</Box>
 
-						<Box
-							display='flex'
-							alignItems='center'
-							justifyContent='space-between'
-							sx={{
-								my: 1
-							}}
-						>
-							<Typography>Lost Items</Typography>
-							<Typography>{counts?.lostItems}</Typography>
-						</Box>
-
-						<Box
-							display='flex'
-							alignItems='center'
-							justifyContent='space-between'
-							sx={{
-								my: 1
-							}}
-						>
-							<Typography>Found Items</Typography>
-							<Typography>{counts?.foundItems}</Typography>
-						</Box>
-
-						<Box
-							display='flex'
-							alignItems='center'
-							justifyContent='space-between'
-							sx={{
-								my: 1
-							}}
-						>
-							<Typography>Claimed Items</Typography>
-							<Typography>{counts?.claimedItems}</Typography>
-						</Box>
+							<Box
+								display='flex'
+								alignItems='center'
+								justifyContent='space-between'
+								sx={{
+									my: 1
+								}}
+							>
+								<Typography>Claimed Items</Typography>
+								<Typography>{counts?.claimedItems}</Typography>
+							</Box>
+						</div>
 					</Grid>
 
 					{/* side */}
@@ -198,6 +202,9 @@ const MyProfile = () => {
 							},
 							p: {
 								md: 1
+							},
+							py: {
+								xs: 4
 							}
 						}}
 					>
@@ -207,6 +214,36 @@ const MyProfile = () => {
 								<LostItemCard key={item.id} item={item} />
 							))}
 						</Stack>
+						<div className='text-center mb-8'>
+							<Button
+								component={Link}
+								href={'/my-profile/lost-items'}
+								sx={{
+									mt: 2
+								}}
+							>
+								See All
+							</Button>
+						</div>
+
+						{/* found items */}
+						<SectionTitle title='Found Items' />
+						<Stack justifyContent='center' alignItems='center' flexWrap='wrap' direction='row' gap={3}>
+							{foundItems?.map((item: TFoundItem) => (
+								<LostItemCard key={item.id} item={item} />
+							))}
+						</Stack>
+						<div className='text-center'>
+							<Button
+								component={Link}
+								href={'/my-profile/found-items'}
+								sx={{
+									mt: 2
+								}}
+							>
+								See All
+							</Button>
+						</div>
 					</Grid>
 				</Grid>
 			</Container>
